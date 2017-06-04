146b1eff95f3d1cd7cc9c5cbd807b394504fde5d
MickeySeamark <Michael.seamark@gmail.com>
2017-06-04 23:28:25 +1000

    adding third-party scripts i dont fully understand

---
+++ Not Dead Yet!/Assets/3rd Party/scripts/GameOver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	public Text scoreText;
+	public Image backgroundImage;
+
+	public bool isShown = false;
+
+	private float transistion = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isShown)
+			return;
+
+		transistion += Time.deltaTime;
+		backgroundImage.color = Color.Lerp (new Color (0, 0, 0, 0), Color.black, transistion);
+	}
+
+	public void ToggleGameOverScreen (float score){
+		gameObject.SetActive (true);
+		scoreText.text = ((int)score).ToString ();
+		isShown = true;
+
+	}
+
+	public void Retry (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+
+	}
+
+	public void ToMenu(){
+
+		SceneManager.LoadScene("Menu");
+	}
+}
---
+++ Not Dead Yet!/Assets/3rd Party/scripts/Score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public float score = 0.0f;
+
+	public float pointsPerSecond;
+
+	public Text scoreText;
+
+	public GameOver gameOver;
+
+	private bool isDead = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isDead)
+			return;
+		score += pointsPerSecond * Time.deltaTime;
+		scoreText.text = ((int)score).ToString ();
+	}
+	public void OnDeath(){
+		isDead = true;
+		if(PlayerPrefs.GetFloat("Highscore") < score)
+			PlayerPrefs.SetFloat ("Highscore", score);
+		gameOver.ToggleGameOverScreen (score);{
+	}
+}
+
+}
